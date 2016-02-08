df00b169528f86749933473d7d26be6ebdf2c3ea
Pavel Batanov <batanov@banki.ru>
2016-02-08 12:10:18 +0300

    Lazy client decorator

---
+++ src/ScayTrase/Api/Rpc/LazyResponseCollection.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: batanov.pavel
+ * Date: 08.02.2016
+ * Time: 10:34
+ */
+
+namespace ScayTrase\Api\Rpc;
+
+final class LazyResponseCollection implements \IteratorAggregate, ResponseCollectionInterface
+{
+    /** @var bool */
+    private $initialized = false;
+    /** @var RpcRequestInterface[] */
+    private $requests = [];
+    /** @var  RpcClientInterface */
+    private $client;
+    /** @var  ResponseCollectionInterface */
+    private $collection;
+
+    /**
+     * LazyResponseCollection constructor.
+     *
+     * @param RpcClientInterface $client
+     */
+    public function __construct(RpcClientInterface $client) { $this->client = $client; }
+
+
+    /** {@inheritdoc} */
+    public function getResponse(RpcRequestInterface $request)
+    {
+        if (!$this->initialized) {
+            $this->init();
+        }
+
+        return $this->collection->getResponse($request);
+    }
+
+    private function init()
+    {
+        $this->collection  = $this->client->invoke($this->requests);
+        $this->requests    = null;
+        $this->initialized = true;
+    }
+
+    /** {@inheritdoc} */
+    public function count()
+    {
+        if (!$this->initialized) {
+            $this->init();
+        }
+
+        return $this->collection->count();
+    }
+
+    public function append(RpcRequestInterface $request)
+    {
+        if ($this->initialized) {
+            throw  new \LogicException('Cannot add request to initialized lazy collection');
+        }
+
+        $this->requests[] = $request;
+    }
+
+    public function isFrozen()
+    {
+        return $this->initialized;
+    }
+
+    /** {@inheritdoc} */
+    public function getIterator()
+    {
+        if (!$this->initialized) {
+            $this->init();
+        }
+
+        return $this->collection;
+    }
+}
---
+++ src/ScayTrase/Api/Rpc/LazyRpcClient.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: batanov.pavel
+ * Date: 08.02.2016
+ * Time: 10:31
+ */
+
+namespace ScayTrase\Api\Rpc;
+
+final class LazyRpcClient implements RpcClientInterface
+{
+    /** @var  LazyResponseCollection */
+    private $collection;
+    /** @var  RpcClientInterface */
+    private $decoratedClient;
+
+    /**
+     * LazyRpcClient constructor.
+     *
+     * @param RpcClientInterface $decoratedClient
+     */
+    public function __construct(RpcClientInterface $decoratedClient)
+    {
+        $this->decoratedClient = $decoratedClient;
+        $this->collection      = new LazyResponseCollection($decoratedClient);
+    }
+
+    public function invoke($calls)
+    {
+        // Reset collection if previous was already sent
+        if ($this->collection->isFrozen()) {
+            $this->collection = new LazyResponseCollection($this->decoratedClient);
+        }
+
+        if (!is_array($calls)) {
+            $calls = [$calls];
+        }
+
+        foreach ($calls as $call) {
+            $this->collection->append($call);
+        }
+
+        return $this->collection;
+    }
+}
